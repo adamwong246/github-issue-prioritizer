4832806f45179aa34080db73de0f310cc7331990
Adam Wong <adamwong246@gmail.com>
2014-09-10 15:28:43 -0700

    added calculated rank to hash. Fixes #4

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 var fastmatter = require('fastmatter');
 var GitHubApi = require("github");
+var moment = require('moment');
+moment().format();
+
+var priority = function(h){
+    var days = moment(h.due_date).diff(moment(), 'days');
+    return h.business_value + h.technical_value + h.completion + days;
+};
 
 var github = new GitHubApi({
     version: "3.0.0",
@@ -13,6 +20,9 @@ github.issues.repoIssues({
         JSON.stringify(
             res.map(function(h){
                 h.priority = fastmatter(h.body);
+                return h;
+            }).map(function(h){
+                h.priority.calculated = priority(h.priority.attributes);
                 return h;
             })
         )
