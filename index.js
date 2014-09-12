add6d6bbe05a45091285224101ae0b5006baa786
Adam Wong <adamwong246@gmail.com>
2014-09-11 17:16:55 -0700

    add support for blockers and more tests

--- index.js
+++ index.js
@@ -14,9 +14,41 @@ var GIP = function (results) {
 
 GIP.prototype.output = function(){
 
+    var this2 = this;
+
     var prioritizeIssue = function(h){
+
+        var calculatedBlockers = function(h){
+
+            if (Object.prototype.toString.call( h.blockers ) === '[object Array]') {
+
+                return _.reduce(h.blockers, function(memo, num){
+
+                    var d = _.filter(this2.parsed, function(obj){
+                        return obj.number == num;
+                    })[0];
+
+                    if ( typeof d !== 'undefined' && d ){
+                        return memo + prioritizeIssue(d.priority.attributes);
+                    } else {
+                        return memo + 0;
+                    }
+
+                }, 0);
+
+            } else {
+                return 0;
+            }
+        };
+
+
         var days = moment(h.due_date).diff(moment(), 'days');
-        return (h.points || 0) + (h.business_value || 0) + (h.technical_value || 0) + (h.completion || 0) + days;
+        return (h.points || 0) +
+            (h.business_value || 0) +
+            (h.technical_value || 0) +
+            (h.completion || 0) +
+            calculatedBlockers(h) +
+            days;
     };
 
     return _.map(this.parsed, function(h){
