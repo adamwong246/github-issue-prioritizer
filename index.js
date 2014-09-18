90ffeb7d608d2b4fc151ffa3f018db277d9b7d85
Adam Wong <adamwong246@gmail.com>
2014-09-18 09:11:15 -0700

    cleanup

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-var fs = require('fs');
-var path = require('path')
 var fastmatter = require('fastmatter');
-var GitHubApi = require("github");
 var moment = require('moment');
-var jade = require('jade');
-var eyes = require('eyes');
 var _ = require('lodash');
 moment().format();
 
@@ -61,67 +56,3 @@ GIP.prototype.output = function(){
 };
 
 module.exports = GIP;
-
-//////////////////////////////////
-    
-// var config = {
-//     // "cache": "./cache.json",
-//     // "input": "./tmp/cache.json",
-//     "output": "./tmp/out.html"
-//     // "output": "./tmp/out.json"
-// };
-
-// var githubRepoConfigs = {
-//     user: "hubbubhealth",
-//     repo: "hubbub-main"
-// };
-
-// var githubAuthConfigs = {
-//     type: "oauth",
-//     token: 'xxx'
-// };
-
-// var githubApiConfigs = {
-//     version: "3.0.0",
-//     // debug: true
-// };
-
-// var raw;
-
-// var outputer = function(results, outputConfig){
-//     if (typeof outputConfig === 'undefined') {
-//         console.log(JSON.stringify(raw, null, 1));
-//     } else {
-//         console.log("writing to: " + outputConfig);
-//         switch(path.extname(outputConfig)) {
-//         case '.json':
-//             fs.writeFileSync(outputConfig, JSON.stringify(raw, null, 1));
-//             break;
-//         case '.html':
-//             fs.writeFileSync(outputConfig,
-//                 jade.renderFile('templates/template.jade', {"pretty": true, "results": results, "datetime": new Date()})
-//             );
-//             break;
-//         }
-//     }
-// };
-
-// if (!(typeof config["input"] === 'undefined')) {
-//     console.log("reading from: " + config["input"]);
-
-//     outputer(new GIP(res).output(), config["output"]);
-
-// } else {
-//     console.log("reading from github: ");
-
-//     var github = new GitHubApi(githubApiConfigs);
-//     github.authenticate(githubAuthConfigs);
-//     github.issues.repoIssues(githubRepoConfigs, function(err, res) {
-//         if (!(typeof config["cache"] === 'undefined')) {
-//             fs.writeFileSync(config["cache"], JSON.stringify(res, null, 1));
-//         }
-
-//         outputer(new GIP(res).output(), config["output"]);
-//     });
-
-// }
